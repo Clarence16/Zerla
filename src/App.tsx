c94a2f6d593f9883edb0802942f9e47a3e51c4ce
clarence16 <clarencewanrap@gmail.com>
2022-03-03 10:34:12 +0530

    Margin and padding less body

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import NavBar from './components/NavBar';
 import data from './components/back/Data';
-// import styled from 'styled-components'
-import { Router } from 'react-router-dom';
 function App() {
   const {productItems} = data
   return (
-    <>
       <NavBar/>
-    </>
   );
 }
 
